f07dc1b2fc745ed7de4aae4b780dcf9236ae8367
Sasha <aleksikunya@mail.ru>
2021-01-23 20:39:32 +0300

    task

---
+++ scripts.js
@@ -0,0 +1,149 @@
+class User {
+    constructor(name, email, address, phone) {
+        this.data = {
+          name: name,
+          email: email,
+          address: address,
+          phone: phone        
+        };
+    }
+
+    get() {
+        return this.data;
+    }
+}
+
+class Contacts {
+    constructor() {
+        this.arrContacts = [];
+    }
+
+    add(name, email, address, phone) {
+        const user = new User(name, email, address, phone);
+        const userGet = user.get()
+        this.arrContacts.push({id: this.arrContacts.length, ...userGet});
+    }
+
+    edit(id, name, email, address, phone) {
+        this.arrContacts[id].name = name;
+        this.arrContacts[id].email = email;
+        this.arrContacts[id].address = address;
+        this.arrContacts[id].phone = phone;
+    }
+
+    remove(id) {
+        delete this.arrContacts[id];
+    }
+
+    get() {
+        return this.arrContacts;
+    }
+}
+
+class ContactsApp extends Contacts {
+    constructor() {
+        super();
+
+        this.app = document.createElement('div');
+        this.create();
+        this.get();
+    }
+
+    create() {
+        const self = this;
+
+        document.body.appendChild(this.app);
+        this.app.classList.add('contacts');
+
+        this.app.insertAdjacentHTML('beforeend',
+        `<div class="container">
+            <h1>Список контактов</h1>
+            <div class="input">
+                <input type="text" class="name" placeholder="Имя">
+                <input type="text" class="email" placeholder="Email">
+                <input type="text" class="address" placeholder="Адрес">
+                <input type="text" class="phone" placeholder="Телефон">
+            </div>
+            <ul class="list"></ul>
+            <div class="button">
+                <button class="add">Добавить</button>
+                <button class="reset">Очистить</button>
+            </div>
+        </div>`);
+
+        const btnAdd = document.querySelector('.add');
+        btnAdd.addEventListener('click', () => {
+            const name = document.getElementsByTagName("input")[0];
+            const email = document.getElementsByTagName("input")[1];
+            const address = document.getElementsByTagName("input")[2];
+            const phone = document.getElementsByTagName("input")[3];   
+
+            let regexpEmail = /^([a-z]+[\d-_\.]*){3,}@[a-z]+\.[a-z]{2,11}$/gi,
+                regexpPhone = /\+?\s?\d{2,3}\s?[-(]?\d{2}\s?[-)]?\s?\d{3}[-]?\d{2}[-]?\d{2}[-]?/gi; 
+
+            if (regexpEmail.test(email.value) == false || regexpPhone.test(phone.value) == false) {
+                alert('Ошибка');
+                email.value = phone.value = ''; 
+            } else {
+                self.add(name.value, email.value, address.value, phone.value);
+                console.log(this.arrContacts);
+                if (document.querySelectorAll('li')) {
+                    document.querySelectorAll('li').forEach(element => element.remove());
+                }
+                    
+                this.createContacts();
+
+                name.value = email.value = address.value = phone.value = '';
+            }
+        });
+
+        const btnReset = document.querySelector('.reset');
+        btnReset.addEventListener('click', () => {
+            document.querySelectorAll('li').forEach(element => element.remove());
+            this.arrContacts.splice(0, this.arrContacts.length);
+        });
+    }
+
+    createContacts() {
+        const self = this;
+        
+        this.arrContacts.map(user => {
+            const pencil = document.createElement('i');
+            pencil.classList.add('fa');
+            pencil.classList.add('fa-pencil');
+
+            const trash = document.createElement('i');
+            trash.classList.add('fa');
+            trash.classList.add('fa-trash-o');
+
+            const span = document.createElement('span');
+            span.append(pencil, trash);
+
+            const li = document.createElement('li');
+
+            const p = document.createElement('p');
+            p.innerHTML = 'Имя: ' + user.name + '<br> Email: ' + user.email + '<br> Адрес: ' + user.address + '<br> Телефон: ' + user.phone;
+            li.append(p, span);
+            document.querySelector('ul').append(li);
+
+            trash.addEventListener('click', () => {
+				self.remove(user.id);
+				li.remove();
+            });
+            
+            pencil.addEventListener('click', () => {
+				user.name = prompt('Редактировать имя:', user.name);
+                user.email = prompt('Редактировать email:', user.email);
+                user.address = prompt('Редактировать адрес:', user.address);
+                user.phone = prompt('Редактировать телефон:', user.phone);
+
+                self.edit(user.id, user.name, user.email, user.address, user.phone);
+
+                p.innerHTML = 'Имя: ' + user.name + '<br> Email: ' + user.email + '<br> Адрес: ' + user.address + '<br> Телефон: ' + user.phone;
+                li.append(p, span);
+			});
+		});
+    }
+}
+
+let contactsApp = new ContactsApp();
